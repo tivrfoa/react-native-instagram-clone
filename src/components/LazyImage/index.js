1b518fcce1754cd9a00594cbd70b4b76ace6385e
heyjo <heyjo@notvalid.com>
2019-08-19 19:25:19 -0300

    Animated createAnimatedComponent timing opacity duration

--- src/components/LazyImage/index.js
+++ src/components/LazyImage/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Animated } from 'react-native';
 
 import { Original, Small } from './styles';
+
+const OriginalAnimated = Animated.createAnimatedComponent(Original);
 
 export default function LazyImage({
     smallSource,
     source,
     aspectRatio,
 }) {
+
+    const opacity = new Animated.Value(0);
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true);
+        }, 3000);
+    }, []);
+
+    function handleAnimate() {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    }
+
     return (
         <Small
             source={smallSource}
@@ -15,11 +35,16 @@ export default function LazyImage({
             resizeMode="contain"
             blurRadius={2}>
 
-            <Original
-                source={source}
-                ratio={aspectRatio}
-                resizeMode="contain"
-            />
+            {
+                loaded &&
+                <OriginalAnimated
+                    style={{ opacity }}
+                    source={source}
+                    ratio={aspectRatio}
+                    resizeMode="contain"
+                    onLoadEnd={handleAnimate}
+                />
+            }
         </Small>
     );
 }
